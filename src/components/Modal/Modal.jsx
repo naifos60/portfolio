3bd0bc210771b9bcc90b2ab97738815d093657df
naifos60 <naifos60@hotmail.com>
2023-09-18 21:29:36 +0200

    feature17 responsive modal + id form

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 function Modal({open, onClose, children}) {
     return (
         <div onClick={onClose} 
-        className={`fixed max-h-500 lg:max-h-full top-44 md:top-24 inset-0 overflow-hidden flex justify-center items-center z-30 
+        className={`fixed max-h-screen sm:max-h-full inset-0 overflow-hidden flex justify-center items-center z-30 
         transition-colors ${open ? "visible bg-black/20" : "invisible"}`} >
             <div onClick={e => e.stopPropagation()} 
             className={` bg-gradient-to-b from-[hsl(218,81%,75%)] border border-white rounded-xl inset-0 shadow p-6 transition-all mx-4
