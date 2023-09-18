84819c7c7d529815d6fea43c43a6e19f1eb738d8
naifos60 <naifos60@hotmail.com>
2023-09-19 01:25:35 +0200

    feature18 update link project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -14,7 +14,7 @@ function Project() {
             <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 top-0 md:-top-10'>PORTFOLIO</h2>
             <h3 className='  font-serif italic  mt-12 md:py-2 md:px-2 mb-12 lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>Quelques réalisations</h3>
             <div className="flex md:flex-row flex-wrap mx-auto md:mt-12 py-6 px-4  gap-12 justify-center">
-                <ProjectCard img={cardBookie} title="Bookie" url={'https://bookie-gcah3bcr3-naifos60.vercel.app'}/>
+                <ProjectCard img={cardBookie} title="Bookie" url={'https://bookie-naifos60.vercel.app'}/>
                 <ProjectCard img={cardOhMyFood} title="Oh My Food" url={'https://oh-my-food-p4.vercel.app/'}/>
                 <ProjectCard img={cardSophie} title="Sophie Bluel" url={'https://github.com/naifos60/Portfolio-architecte-sophie-bluel.git'}/>   
                 <ProjectCard img={cardArgent} title="Bank-tree" url={'https://github.com/naifos60/argent-bank-projet11.git'}/>
