4bf04917def1a00d78f48aedf11c638260283d7a
naifos60 <naifos60@hotmail.com>
2023-09-18 17:28:36 +0200

    feature15 update header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -70,7 +70,7 @@ function Header(){
               href="#description"
               className="text-sm uppercase font-semibold hover:text-rose-600"
               onClick={toggleNav}
-              >Déscription
+              >Présentation
             </a>
           </li>
           <li className="block py-3 md:inline md:py-0 md:mr-6">
