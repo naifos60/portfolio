8df576acf2eca3847828d993c5cb10008a544dbb
naifos60 <naifos60@hotmail.com>
2023-09-19 14:23:03 +0200

    feature17 update describe

--- src/components/Describe/Describe.jsx
+++ src/components/Describe/Describe.jsx
@@ -5,8 +5,8 @@ function Describe() {
         <div id="description" className="w-full max-h-auto mt-16 mb-8 lg:my-28 h-auto mx-auto px-10 py-10  lg:p-20 lg:mt-0 flex flex-col items-center bg-describe bg-cover  relative  overflow-hidden rounded-lg">
             <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-5xl  lg:text-9xl text-gray-400 text-center absolute  opacity-20 lg:top-12'>QUI SUIS-JE ?</h2>
                 <h3 className=' font-serif italic  mt-12 md:py-2 md:px-2 mb-12 lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>Présentation</h3>
-                <div className='flex flex-col  items-center justify-center gap-6 lg:mx-30  h-contain my-12 lg:my-4  shadow-lg shadow-white  rounded-xl '>
-                    <article className=" flex flex-col  xl:flex-row font-serif italic px-6 py-12  bg-gradient-to-r from-[hsl(218,81%,75%)] bg-opacity-70 justify-between items-center w-auto max-h-350 md:max-h-screen xl:max-h-full overflow-scroll scroll-smooth">
+                <div className='flex flex-col  items-center justify-center gap-6 lg:mx-30  h-contain my-12 lg:my-4    rounded-xl '>
+                    <article className=" flex flex-col  xl:flex-row font-serif italic px-6 py-12  bg-gradient-to-l from-[hsl(218,81%,75%)] bg-opacity-70 justify-between items-center w-auto max-h-350 md:max-h-screen xl:max-h-full overflow-scroll scroll-smooth">
                         <img src={logoRs} alt="logo dev" className='rounded-full w-56 h-56 mx-auto'/>
                         <div className="md:flex flex-col justify-center text-xl md:text-justify text-white ml-12 py-8"> 
                         <p>
@@ -23,7 +23,7 @@ function Describe() {
                             Parmis lesquels HTML5, CSS3, Javascript et Typescript mais aussi React, Node, Tailwind, Sass, Jest et plusieurs autres compétences comme le référencement, la gestion de projets Agile, la méthode SCRUM et les tableaux kanban...
                         </p><br/> 
                         <p>
-                            Vous trouverez plus de détails sur quelqu'une de ses compétences <a href='#competences' className='font-bold text-xl text-[hsl(218,81%,75%)] cursor-pointer underline hover:text-black hover:scale-120'>ICI</a> ainsi que certains projets également <a href='#projets' className='font-bold text-xl text-[hsl(218,81%,75%)] cursor-pointer underline hover:text-black'>ICI</a> . 
+                            Vous trouverez plus de détails sur quelqu'une de ses compétences <a href='#competences' className='font-bold text-xl text-gray-800 cursor-pointer underline hover:text-red-600 hover:scale-120'>ICI</a> ainsi que certains projets également <a href='#projets' className='font-bold text-xl text-gray-800 cursor-pointer underline hover:text-red-600'>ICI</a> . 
                             Tant de compétence qui Je l'espère me permettrons de trouver une société qui me donnera une chance et me permettra d'augmenter mes compétences tout en partageant mes connaissances au sein d'une équipe dans un cadre professionnel.
                         </p>
                         </div>
