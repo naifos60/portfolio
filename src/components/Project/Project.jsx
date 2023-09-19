b0bf91ab6cdec7192c60700223f8bad0f862e7cc
naifos60 <naifos60@hotmail.com>
2023-09-19 14:40:43 +0200

    feature19  refactor project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -16,10 +16,10 @@ function Project() {
             <div className="flex md:flex-row flex-wrap mx-auto md:mt-12 py-6 px-4  gap-12 justify-center">
                 <ProjectCard img={cardBookie} title="Bookie" url={'https://bookie-naifos60.vercel.app'}/>
                 <ProjectCard img={cardOhMyFood} title="Oh My Food" url={'https://oh-my-food-p4.vercel.app/'}/>
+                <ProjectCard img={cardNina} title="Nina Carducci photo" url={'https://naifos60.github.io/ninacarducci.github.io-main/'}/>
+                <ProjectCard img={cardKasa} title="Kasa" url={'https://kasa-p8.vercel.app/'}/> 
                 <ProjectCard img={cardSophie} title="Sophie Bluel" url={'https://github.com/naifos60/Portfolio-architecte-sophie-bluel.git'}/>   
                 <ProjectCard img={cardArgent} title="Bank-tree" url={'https://github.com/naifos60/argent-bank-projet11.git'}/>
-                <ProjectCard img={cardKasa} title="Kasa" url={'https://kasa-p8.vercel.app/'}/> 
-                <ProjectCard img={cardNina} title="Nina Carducci photo" url={'https://naifos60.github.io/ninacarducci.github.io-main/'}/>            
             </div>
         </section>
     )
