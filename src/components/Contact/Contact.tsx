7c070ea82f4263e3ecfc0b8b9892df467f331491
naifos60 <naifos60@hotmail.com>
2023-09-30 23:34:56 +0200

    feature03I18next skills + contact translate

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,31 +1,33 @@
+import {useTranslation} from "react-i18next"
 const tel = ('./assets/images/tel.svg') as string;
 const linkedin = ('./assets/images/linkedin.svg') as string;
 const email = ('./assets/images/email.svg') as string;
 
 function Contact() {
+  const { t } = useTranslation()
     return (
         <div>
           <section id="contact" className="text-gray-700 body-font relative  mx-auto  lg:mt-0 flex flex-col items-center w-full  lg:h-full mb-12">  
-            <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 md:-top-10'>CONTACT</h2>
-            <h3 className='  font-serif italic  mt-12 md:py-2 md:px-2  lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>N'hésitez pas à me contacter</h3>
+            <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 md:-top-10'>{t('contact.title1')}</h2>
+            <h3 className='  font-serif italic  mt-12 md:py-2 md:px-2  lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>{t('contact.title2')}</h3>
             <div className="flex justify-center w-full h-auto items-center p-2 mx-auto relative my-6">
                 <div className="flex flex-col gap-6 justify-center items-center w-full">
                   <div className='flex flex-row items-center text-white justify-center'>
                     <img className='h-12 w-12 bg-white aboslute top-0 left-0' src={tel} alt="logo telephone"/>
                     <a className="flex mx-auto mb-2 lg:-text-center  rounded bg-gradient-to-r from-[hsl(218,81%,75%)] px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-[hsl(218,81%,95%)] hover:text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-[hsl(218,81%,95%)] hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]  md:mb-0 cursor-pointer"  href='tel:+33671451172'>
-                      CLIQUEZ POUR APPELER
+                      {t('contact.call')}
                     </a>
                   </div>
                   <div className='flex flex-row items-center text-white'>
                     <img className='h-12 w-12 bg-white aboslute top-0 left-0 ' src={email} alt="logo email"/>
                     <a className="flex mx-auto mb-2 lg:-text-center  rounded bg-gradient-to-r from-[hsl(218,81%,75%)] px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-[hsl(218,81%,95%)] hover:text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-[hsl(218,81%,95%)] hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]  md:mb-0 cursor-pointer" href="mailto:masenkodrummer@hotmail.com">
-                      envoyer un email
+                      {t('contact.mail')}
                     </a>
                   </div>
                   <div className='flex flex-row items-center text-white'>
                     <img className='h-12 w-12 bg-white aboslute top-0 left-0' src={linkedin} alt="logo linkedin"/>
                     <a className="flex mx-auto mb-2 lg:-text-center  rounded bg-gradient-to-r from-[hsl(218,81%,75%)] px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-[hsl(218,81%,95%)] hover:text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-[hsl(218,81%,95%)] hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]  md:mb-0 cursor-pointer" href="https://www.linkedin.com/in/sofiane-rhrara-369906250">
-                      Par linkedin
+                      {t('contact.linkedin')}
                     </a>
                   </div>
                 </div>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,13 +1,15 @@
 import Progress from '../Progress/Progress'
 import datas from '../../data/skills.json'
+import {useTranslation} from "react-i18next"
 const bureau = ('./assets/images/background/5.webp')
 
 function Skills() {
     const data = datas
+    const { t } = useTranslation()
     return(
         <section id='competences' className='mx-auto   flex flex-col items-center relative w-full h-full lg:mb-24'>
-            <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 md:-top-10'>A PROPOS</h2>
-            <h3 className='font-serif italic  mt-12 md:py-2 md:px-2 mb-12 lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>Mes compétences à votre service</h3>
+            <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 md:-top-10'>{t('skills.title1')}</h2>
+            <h3 className='font-serif italic  mt-12 md:py-2 md:px-2 mb-12 lg:mb-0 text-3xl md:text-6xl text-white text-center z-10'>{t('skills.title2')}</h3>
             <div className='flex flex-col lg:flex-row justify-center items-center w-full h-full lg:pt-12'>
                     <img src={bureau} alt="bureau avec ordinateur dessus" className='hidden lg:flex w-auto h-auto lg:h-555 lg:w-2/4 object-contains lg:my-0 lg:flex-start px-8 lg:pl-0 my-8 lg:mx-0 lg:border-r-2 pb-12 lg:pb-0 lg:pr-36 lg:rounded '/>
                 <div className='flex flex-col w-full items-center justify-center gap-6 lg:pl-24 h-auto mb-4 lg:pt-4 '>
@@ -15,7 +17,7 @@ function Skills() {
                         <Progress key={el.id} percent={el.percent} color={el.color} logo={el.logo} logo2={el.logo2} skill={el.title}>
                          {<ul className='text-left'>
                             {el.skill.map((e, index) => (
-                                <li className='my-4 text-xs list-decimal' key={index}>{e}</li>
+                                <li className='my-4 text-xs list-decimal' key={index}>{t(e)}</li>
                             ))}
                           </ul>}
                         </Progress>
