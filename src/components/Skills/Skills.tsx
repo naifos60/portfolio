bd2697ac7e2b6edabe95a6307a5eeabfe9197bbc
naifos60 <naifos60@hotmail.com>
2023-10-01 21:57:37 +0200

    feature07I18next remove data json file + add data in translate json file

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,11 +1,11 @@
 import Progress from '../Progress/Progress'
-import datas from '../../data/skills.json'
 import {useTranslation} from "react-i18next"
 const bureau = ('./assets/images/background/5.webp')
 
 function Skills() {
-    const data = datas
     const { t } = useTranslation()
+    const dataSkills = t('dataSkills', { returnObjects: true });
+    const dat: any = dataSkills
     return(
         <section id='competences' className='mx-auto   flex flex-col items-center relative w-full h-full lg:mb-24'>
             <h2 className='font-lora font-bold mt-10 md:py-2 md:px-2 mb-12 lg:mb-0 text-6xl md:text-9xl text-gray-400 text-center absolute  opacity-20 md:-top-10'>{t('skills.title1')}</h2>
@@ -13,11 +13,11 @@ function Skills() {
             <div className='flex flex-col lg:flex-row justify-center items-center w-full h-full lg:pt-12'>
                     <img src={bureau} alt="bureau avec ordinateur dessus" className='hidden lg:flex w-auto h-auto lg:h-555 lg:w-2/4 object-contains lg:my-0 lg:flex-start px-8 lg:pl-0 my-8 lg:mx-0 lg:border-r-2 pb-12 lg:pb-0 lg:pr-36 lg:rounded '/>
                 <div className='flex flex-col w-full items-center justify-center gap-6 lg:pl-24 h-auto mb-4 lg:pt-4 '>
-                    {data.map((el) => (
+                    {dat.map((el: any) => (
                         <Progress key={el.id} percent={el.percent} color={el.color} logo={el.logo} logo2={el.logo2} skill={el.title}>
                          {<ul className='text-left'>
-                            {el.skill.map((e, index) => (
-                                <li className='my-4 text-xs list-decimal' key={index}>{t(e)}</li>
+                            {dat.map((e: any, index: any) => (
+                                <li className='my-4 text-xs list-decimal' key={index}>{t(e.skill)}</li>
                             ))}
                           </ul>}
                         </Progress>
